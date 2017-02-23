6dd93d3a7b14335a594a8485dc2dec5f10d9f1f6
Emerald Laurente <emerald.laurente@cdk.com>
2017-02-22 19:31:32 -0800

    Added context description

--- src/com/laurente/programming/arrays/PermuteArray.java
+++ src/com/laurente/programming/arrays/PermuteArray.java
@@ -3,7 +3,12 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Created by laurente on 2/21/17.
+ * EPI 6.9 (pp. 74-75)
+ * A permutation can be viewed as a rearrangement of elements of a set/sequence to a new one. For example,
+ * there are 24 permutations of {a, b, c, d}, as n!/(n-r)! describes.
+ *
+ * Given an array of permutation definition P and an array of elements A, apply P to A. For example,
+ * let P = {2, 0, 1, 3} applied to A = {a, b, c, d} => {b, c, a, d}
  */
 public class PermuteArray {
     public static List<Integer> apply(List<Integer> permutations, List<Integer> original) {
