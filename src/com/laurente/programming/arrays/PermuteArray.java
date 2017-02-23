d86ce3e342fe012e26cd9ad9022bd7a253013be6
Emerald Laurente <emerald.laurente@cdk.com>
2017-02-23 14:53:22 -0800

    Added naive algorithm for obtaining next increment of a permutation

--- src/com/laurente/programming/arrays/PermuteArray.java
+++ src/com/laurente/programming/arrays/PermuteArray.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * EPI 6.9 (pp. 74-75)
- * A permutation can be viewed as a rearrangement of elements of a set/sequence to a new one. For example,
- * there are 24 permutations of {a, b, c, d}, as n!/(n-r)! describes.
- *
- * Given an array of permutation definition P and an array of elements A, apply P to A. For example,
- * let P = {2, 0, 1, 3} applied to A = {a, b, c, d} => {b, c, a, d}
- */
 public class PermuteArray {
+    /**
+     * EPI 6.9 (pp. 74-75)
+     * A permutation can be viewed as a rearrangement of elements of a set/sequence to a new one. For example,
+     * there are 24 permutations of {a, b, c, d}, as n!/(n-r)! describes.
+     *
+     * Given an array of permutation definition P and an array of elements A, apply P to A. For example,
+     * let P = {2, 0, 1, 3} applied to A = {a, b, c, d} => {b, c, a, d}
+     */
     public static List<Integer> apply(List<Integer> permutations, List<Integer> original) {
         if (permutations.size() != original.size()) {
             return null;
@@ -22,5 +22,33 @@ public class PermuteArray {
         }
 
         return result;
+    }
+
+    /**
+     * EPI
+     */
+    public static List<Integer> next(List<Integer> currentPermutation) {
+        int lastIndex = currentPermutation.size() - 1;
+        List<Integer> next = new ArrayList<>(Collections.nCopies(currentPermutation.size(), 0));
+        boolean increment = true;
+        for (int i = lastIndex; i > 0; i--) {
+            if ((currentPermutation.get(i) + 1) % 10 == 0) {
+                next.set(i, 0);
+                next.set(i - 1, currentPermutation.get(i - 1) + 1);
+            }
+            else {
+                if (increment) {
+                    next.set(i, currentPermutation.get(i) + 1);
+                    increment = false;
+                }
+                next.set(i - 1, currentPermutation.get(i - 1));
+            }
+        }
+
+        if (next.get(0) == 10) {
+            next.clear();
+        }
+
+        return next;
     }
 }
--- src/test/java/PermuteArrayTest.java
+++ src/test/java/PermuteArrayTest.java
@@ -11,9 +11,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class PermuteArrayTest {
 
     @Test
-    public void test() {
-        List<Integer> result = PermuteArray.apply(Arrays.asList(2, 0, 1, 3), Arrays.asList(1, 2, 3, 4));
-        System.out.println(result);
+    public void appliesAPermutationOnAnArray() {
+        List<Integer> A = Arrays.asList(1, 2, 3, 4);
+        List<Integer> result = PermuteArray.apply(Arrays.asList(2, 0, 1, 3), A);
+        System.out.println(A + " => " + result);
         assertEquals(4, result.size());
     }
+
+    @Test
+    public void getsTheNextPermutationOfZeroZeroZero() {
+        List<Integer> A = Arrays.asList(0, 0, 0);
+        List<Integer> result = PermuteArray.next(A);
+        System.out.println(A + " => " + result);
+        assertEquals(Arrays.asList(0, 0, 1), result);
+    }
+
+    @Test
+    public void getsTheNextPermutationOfOneThreeNine() {
+        List<Integer> A = Arrays.asList(1, 3, 9);
+        List<Integer> result = PermuteArray.next(A);
+        System.out.println(A + " => " + result);
+        assertEquals(Arrays.asList(1, 4, 0), result);
+    }
+
+    @Test
+    public void emptyArrayOnLastPermutation() {
+        List<Integer> A = Arrays.asList(9, 9, 9);
+        List<Integer> result = PermuteArray.next(A);
+        System.out.println(A + " => " + result);
+        assertEquals(Arrays.asList(), result);
+    }
+
 }
